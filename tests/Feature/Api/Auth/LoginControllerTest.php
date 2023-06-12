604810f8a6edda7bd050b1d7abbf0b1aae4f75e1
Daniel Newns <dan@jump24.co.uk>
2023-06-12 22:09:55 +0100

    new test

--- tests/Feature/Api/Auth/LoginControllerTest.php
+++ tests/Feature/Api/Auth/LoginControllerTest.php
@@ -18,13 +18,24 @@ final class LoginControllerTest extends TestCase
     use RefreshDatabase;
 
     #[Test]
-    public function it_throws_validation_error_when_not_complete_login(): void
+    public function it_throws_validation_error_when_not_actual_user_trying_to_login(): void
     {
         $postData = RegisterUserRequestFactory::new()->create();
 
         $response = $this->postJson('/api/auth/login', $postData);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
 
+    #[Test]
+    public function it_throws_validation_error_when_actual_user_trying_to_login_with_invalid_password(): void
+    {
+        User::factory()->create(['password' => '123sdfdsfdsf3']);
+
+        $postData = RegisterUserRequestFactory::new()->create();
+
+        $response = $this->postJson('/api/auth/login', $postData);
+
+        $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
     }
 }
