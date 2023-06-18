f99014f3181695ec37c6de561258ab0badf44691
Daniel Newns <dan@jump24.co.uk>
2023-06-18 09:38:42 +0100

    ulids for model changes

--- app/Models/Location.php
+++ app/Models/Location.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -14,7 +15,7 @@ class Location extends Model
 {
     use SoftDeletes;
     use HasFactory;
-    use HasUuids;
+    use HasUlids;
 
     protected $fillable = [
         'id',
--- app/Models/LocationImage.php
+++ app/Models/LocationImage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -12,6 +13,7 @@ class LocationImage extends Model
 {
     use SoftDeletes;
     use HasFactory;
+    use HasUlids;
 
     protected $fillable = [
         'id',
