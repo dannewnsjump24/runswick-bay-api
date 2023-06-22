d9e71cfdc21643ef578def2232a31de8e215bed2
Daniel Newns <dan@jump24.co.uk>
2023-06-22 16:40:57 +0100

    adding pagination test in

--- tests/Feature/Api/Trips/IndexControllerTest.php
+++ tests/Feature/Api/Trips/IndexControllerTest.php
@@ -102,4 +102,45 @@ class IndexControllerTest extends TestCase
 
         $response->assertJsonCount(4, 'data');
     }
+
+    #[Test]
+    public function it_returns_meta_data_for_pagination_when_enough_trips_exist(): void
+    {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        Trip::factory()->times(25)->create(
+            [
+                'owner_id' => $user->id,
+            ]
+        );
+
+        $userTwo = User::factory()->create();
+
+        Trip::factory()->times(4)->create(
+            [
+                'owner_id' => $userTwo->id,
+            ]
+        );
+
+        $response = $this->getJson(route('api.trips.index'));
+
+        $response->assertOk();
+
+        $response->assertJsonCount(15, 'data');
+
+        $response->assertJson([
+                'meta' => [
+                    "current_page" => 1,
+                    "from" => 1,
+                    "last_page" => 2,
+                    "path" => "http://localhost/api/trips",
+                    "per_page" => 15,
+                    "to" => 15,
+                    "total" => 25,
+                ],
+            ]
+        );
+    }
 }
