2175f7651d098510318a054d65556619adf0c69b
Daniel Newns <dan@jump24.co.uk>
2023-07-20 07:19:36 +0100

    adding in error checking for tests

--- app/Http/Controllers/Api/Location/CreateController.php
+++ app/Http/Controllers/Api/Location/CreateController.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Api\Location;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Locations\StoreRequest;
 
 final class CreateController extends Controller
 {
-    public function __invoke(): void
+    public function __invoke(StoreRequest $request): void
     {
     }
 }
--- tests/Feature/Api/Location/CreateControllerTest.php
+++ tests/Feature/Api/Location/CreateControllerTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\Location;
 
+use App\Domain\Trips\Models\Trip;
 use App\Models\User;
+use Illuminate\Foundation\Testing\WithFaker;
 use Laravel\Sanctum\Sanctum;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Test;
@@ -13,6 +15,8 @@ use Tests\TestCase;
 #[Group("Locations")]
 class CreateControllerTest extends TestCase
 {
+    use WithFaker;
+
     #[Test]
     public function it_wont_allow_an_unauthenticated_user_to_create_a_location(): void
     {
@@ -31,5 +35,44 @@ class CreateControllerTest extends TestCase
         $response = $this->postJson(route('api.locations.store'), $postData);
 
         $response->assertUnprocessable();
+
+        $response->assertJsonValidationErrors(
+            [
+                'lat',
+                'long',
+                'name',
+                'trip_id',
+            ]
+        );
+    }
+
+    #[Test]
+    public function it_wont_allow_a_user_to_create_a_location_against_a_trip_they_dont_own(): void
+    {
+        $user = User::factory()->create();
+
+        $userTwo = User::factory()->create();
+
+        /** @var Trip $trip */
+        $trip = Trip::factory()->create(
+            [
+                'owner_id' => $userTwo->id,
+            ]
+        );
+
+        Sanctum::actingAs($user);
+
+        $locationData = [
+            'trip_id' => $trip->id,
+            'name' => 'south wales',
+            'lat' => $this->faker->latitude(),
+            'long' => $this->faker->longitude(),
+        ];
+
+        $response = $this->postJson(route('api.locations.store'), $locationData);
+
+        $response->assertUnprocessable();
+
+        $response->assertJsonValidationErrorFor('trip_id');
     }
 }
