0ee8c134a84ff6c4e1606fe545317cbdc2a569ec
Daniel Newns <dan@jump24.co.uk>
2023-09-02 21:00:29 +0100

    adding in location form field and adding links to trips

--- app/Filament/Resources/LocationResource.php
+++ app/Filament/Resources/LocationResource.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Filament\Resources;
 
 use App\Domain\Locations\Models\Location;
+use App\Domain\Trips\Models\Trip;
 use App\Filament\Resources\LocationResource\Pages;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
 
 class LocationResource extends Resource
 {
@@ -24,6 +28,14 @@ class LocationResource extends Resource
     {
         return $form
             ->schema([
+                Select::make('trip_id')
+                    ->relationship(name: 'trip', titleAttribute: 'name')
+                    ->required(),
+                TextInput::make('name')->required(),
+                TextInput::make('latitude')->numeric()
+                    ->inputMode('decimal')->required(),
+                TextInput::make('longitude')->numeric()
+                    ->inputMode('decimal')->required(),
             ]);
     }
 
@@ -31,12 +43,14 @@ class LocationResource extends Resource
     {
         return $table
             ->columns([
-                Tables\Columns\TextColumn::make('trip.name')->label('Trip')->weight(FontWeight::Bold),
+                Tables\Columns\TextColumn::make('trip.name')
+                    ->label('Trip')
+                    ->weight(FontWeight::Bold)
+                    ->url(fn (Model $record): string => route('filament.admin.resources.trips.edit', $record->trip_id)),
                 Tables\Columns\TextColumn::make('name')->weight(FontWeight::Bold),
-                Tables\Columns\TextColumn::make('longitude')->date('Y-m-d'),
-                Tables\Columns\TextColumn::make('latitude')->date('Y-m-d'),
-            ])
-            ->filters([
+                Tables\Columns\TextColumn::make('longitude'),
+                Tables\Columns\TextColumn::make('latitude'),
+                Tables\Columns\TextColumn::make('images_count')->counts('images'),
 
             ])
             ->actions([
@@ -49,7 +63,8 @@ class LocationResource extends Resource
             ])
             ->emptyStateActions([
                 Tables\Actions\CreateAction::make(),
-            ]);
+            ])
+            ->striped();
     }
 
     public static function getRelations(): array
