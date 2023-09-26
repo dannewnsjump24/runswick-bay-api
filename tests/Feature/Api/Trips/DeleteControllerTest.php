17a58452e6ca3e5ba866b51a0dcff9acf5bc6f84
Daniel Newns <dan@jump24.co.uk>
2023-09-26 21:24:02 +0100

    adding new test

--- tests/Feature/Api/Trips/DeleteControllerTest.php
+++ tests/Feature/Api/Trips/DeleteControllerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Api\Trips;
 
+use App\Domain\Locations\Models\Location;
 use App\Domain\Trips\Models\Trip;
 use App\Models\User;
 use Laravel\Sanctum\Sanctum;
@@ -62,5 +63,43 @@ class DeleteControllerTest extends TestCase
                 'owner_id' => $user->id,
             ]
         );
+    }
+
+    #[Test]
+    public function it_does_delete_the_locations_associated_with_a_trip(): void
+    {
+        $user = User::factory()->create();
+
+        Sanctum::actingAs($user);
+
+        /** @var Trip $trip */
+        $trip = Trip::factory()->has(Location::factory()->count(3))->create(
+            [
+                'owner_id' => $user->id,
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            Location::class,
+            3
+        );
+
+        $response = $this->deleteJson(route('api.trips.delete', $trip->id));
+
+        $response->assertNoContent();
+
+        $this->assertSoftDeleted(
+            Trip::class,
+            [
+                'id' => $trip->id,
+                'owner_id' => $user->id,
+            ]
+        );
+
+        $this->assertDatabaseCount(
+            Location::class,
+            0
+        );
+
     }
 }
